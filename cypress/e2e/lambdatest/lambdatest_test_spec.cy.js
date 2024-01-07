34067e7dc8daaa416e0a884103692c654e1dcccc
Kailash Chander Pathak <you@example.com>
2024-01-07 19:52:03 +0530

    first commit

---
+++ cypress/e2e/lambdatest/lambdatest_test_spec.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+describe("Lambdatest Login ",() => {
+  it("Open the URL", () => {
+    cy.visit("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
+  });
+  it("Login into the application", () => {
+    cy.get('[id="input-email"]').type("lambdatest@yopmail.com");
+    cy.get('[id="input-password"]').type("lambdatest");
+    cy.get('[type="submit"]').eq(0).click();
+  });
+  it("Search the Product", () => {
+    cy.get('[name="search"]').eq(0).type("VAIO");
+    cy.get('[type="submit"]').eq(0).click();
+  });
+  it("Verify Product after search ", () => {
+    cy.contains("Sony VAIO");
+  });
+});
